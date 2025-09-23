a42fc2ec3933da185d5488364782d15e029ee301
Pavel-Yok <pavel@yokweb.com>
2025-09-23 13:11:00 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-console.log("🚀 Test deployment from GitHub CI/CD at", new Date().toISOString());
+console.log("🚀 Test no2 deployment from GitHub CI/CD at", new Date().toISOString());
 
 import express from "express";
 import Stripe from "stripe";
