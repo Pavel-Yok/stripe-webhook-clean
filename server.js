c189eec2ceeab1acfe4398a6b5d6aecd6df95b48
Pavel-Yok <pavel@yokweb.com>
2025-09-28 09:18:55 +0200

    Update server.js

--- server.js
+++ server.js
@@ -285,7 +285,7 @@ app.post("/webhook", bodyParser.raw({ type: "application/json" }), async (req, r
 /* ======================
    Health Check
 ====================== */
-app.get("/", (req, res) => {
+app.get("/webhook", (req, res) => {
   res.send("✅ Stripe Webhook service is running");
 });
 
